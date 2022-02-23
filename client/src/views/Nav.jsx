30ff34d0a1c6c1cf03f7c8548cddcb6061cbf058
Matt Woodside <bmwoodside@live.com>
2022-02-22 19:50:33 -0500

    add navbar view and some of Main view

---
+++ client/src/views/Nav.jsx
@@ -0,0 +1,33 @@
+import { Link } from "react-router-dom";
+import "./nav.css"
+
+
+const Nav = (props) => {
+
+
+    return (
+        <div className="nav-wrapper">
+            <div className="search">
+                <button className="btn-info">ico</button>
+                <input type="text" name="" id="searchbar" />
+            </div>
+
+            <div className="center-icons">
+                <Link to="#"><button className="btn btn-info" id="home">home</button></Link>
+                <Link to="#"><button className="btn btn-info" id="watch">watch</button></Link>
+                <Link to="#"><button className="btn btn-info" id="marketplace">marketplace</button></Link>
+                <Link to="#"><button className="btn btn-info" id="groups">groups</button></Link>
+            </div>
+
+            <div className="nav-right">
+                <Link to="#"><button className="btn btn-info"><img src="" alt="" />Name</button></Link>
+                <Link to="#"><button className="btn btn-info">...</button></Link>
+                <Link to="#"><button className="btn btn-info">bell</button></Link>
+                <Link to="#"><button className="btn btn-info">notify</button></Link>
+                <Link to="#"><button className="btn btn-info">acct</button></Link>
+            </div>
+        </div>
+    )
+}
+
+export default Nav;
