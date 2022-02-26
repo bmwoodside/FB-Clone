a04f638566864d7ab9af762eeebc183c3ac20ffe
Matt Woodside <bmwoodside@live.com>
2022-02-25 20:34:41 -0500

    add user model

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const port = 8000;
+
+const mongoose = require('mongoose');
+
+// This will fire our mongoose.connect statement to initialize our database connection
+require("./server/config/mongoose.config");
+
+app.use(express.json(), express.urlencoded({extended:true}));
+
+const cors = require("cors");
+app.use(cors());
+
+// this sends our app over to the user routes
+require("./server/routes/user.routes")(app);
+
+app.listen(port, ()=>console.log(`running on ${port} is new port!`));
---
+++ server/models/user.model.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    userEmail: {
+        type: String,
+        required: [true, "Email is required."], // this probably goes away with RegEX
+        minlength: [3, "Email requires at least 3 characters."], // this needs RegEX
+    },
+    userPassword: { // oh god how do I implement BCrypt here?
+        type: String,
+        required: [true, "Password is required."],
+        minlength: [3, "Password must be at least 3 characters long."],
+    },
+    // Implement minimum age requirement using their birthday
+    // for now we're just storing the info - need to implement age checking
+    userBirthday: {
+        type: Date,
+    }
+}, { timestamps: true });
+
+module.exports.User = mongoose.model('User', UserSchema);
