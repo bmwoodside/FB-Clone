d567e3db6ca08e5e1525f97fd712a4204af79b5c
Matt Woodside <bmwoodside@live.com>
2022-03-09 18:32:52 -0500

    add virtual confirmPassword, add confirmPassword validation (pre), add bcrypt password hashing (pre)

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,25 +1,47 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const UserSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: [true, "First name is required"]
+    },
+    lastName: {
+        type: String,
+        required: [true, "Last name is required"]
+    },
     userEmail: {
         type: String,
-        required: [true, "Email is required."], // this probably goes away with RegEX
-        minlength: [3, "Email requires at least 3 characters."], // this needs RegEX
+        required: [true, "Email is required."],
+        validate: {
+            validator: val => /^([\w-\.]+@([\w-]+\.)+[\w-]+)?$/.test(val),
+            message: "Please enter a valid email"
+        }
     },
     userPassword: { // oh god how do I implement BCrypt here?
         type: String,
         required: [true, "Password is required."],
-        minlength: [3, "Password must be at least 3 characters long."],
-    },
-    // Implement minimum age requirement using their birthday
-    // for now we're just storing the info - need to implement age checking
-    // ***TEMP DISABLED*** - decide how to implement this (MUI? Self-Coded/validated?)
-    // userBirthday: {
-    //     type: Date, // this type expected syntax is 'yyyy-mm-dd'
-    // },
-    userDisplayName: {
-        type: String
+        minlength: [3, "Password must be at least 3 characters long."]
     }
 }, { timestamps: true });
+
+UserSchema.virtual('confirmPassword')
+    .get( () => this._confirmPassword )
+    .set( value => this._confirmPassword = value );
+
+UserSchema.pre('validate', function(next) {
+    if (this.userPassword !== this.confirmPassword) {
+        this.invalidate('confirmPassword', 'Password must match Confirm Password');
+    }
+    next();
+});
+
+UserSchema.pre('save', function(next) {
+    bcrypt.hash(this.userPassword, 10)
+        .then(hash => {
+            this.userPassword = hash;
+            next();
+        });
+});
 
 module.exports.User = mongoose.model('User', UserSchema);
